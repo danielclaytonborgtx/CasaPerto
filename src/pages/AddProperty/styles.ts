6727f6e8a5a1d2461236e1357c9de7a7fc9ea4fc
danielclayton <danielclaitul@gmail.com>
2025-01-15 10:04:06 -0300

    commit46

--- src/pages/AddProperty/styles.ts
+++ src/pages/AddProperty/styles.ts
@@ -8,7 +8,7 @@ export const AddPropertyContainer = styled.div`
   background-color: #fff;
   border-radius: 8px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  h1 {
+  h1,p {
     text-align: center;
   }
 `;
