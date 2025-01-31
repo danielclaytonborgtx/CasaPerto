4a9d995f779d73f58cf1a32b48ad61e216b6d791
danielclayton <danielclaitul@gmail.com>
2025-01-31 12:34:09 -0300

    commit80

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -15,7 +15,7 @@ const Home: React.FC = () => {
         sua busca simples e eficiente. Acima no cabeçalho temos tres opções, o menu a esquerda onde é possivel fazer o login, após logado você terá acesso ao seu perfil, onde verá os seus imóveis já postados. No meio, tem o botão que muda de venda para aluguel, somente para vizualizar no mapa ou na lista.
         a direita no + é possivel postar o imóvel, somente se estiver logado. 
         abaixo temos um rodapé, onde tem as opções de mapa e de lista.
-        Qualquer falha que hover, tente atualizar a página possivelmente resolverá.
+        Qualquer falha que houver, tente atualizar a página possivelmente resolverá.
       </Description>
     </Container>
   );
